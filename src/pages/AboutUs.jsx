8270a933ee3072b3418155980901ff5ea6809824
Damianos93 <damianosmakris@hotmail.com>
2020-12-10 19:50:48 +0100

    About us

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import AboutSection from "../Components/AboutSection"
+
+
+function Aboutus(props) {
+    return (
+        <>
+           <AboutSection />
+            
+        </>
+    )
+}
+
+
+export default Aboutus
