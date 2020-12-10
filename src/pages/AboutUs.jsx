8af76264cc7823569b3c5222d7531adb2e3a1e04
Damianos93 <damianosmakris@hotmail.com>
2020-12-10 20:07:04 +0100

    4.Faq Section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import AboutSection from "../Components/AboutSection"
+import ServicesSection from "../Components/ServicesSection"
+import FaqSection from "../Components/FaqSection"
 
 
 function Aboutus(props) {
     return (
         <>
            <AboutSection />
-            
+            <ServicesSection />
+            <FaqSection/>
         </>
     )
 }
